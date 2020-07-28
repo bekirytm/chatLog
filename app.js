d30ee4ec1263efa5bebf67b5a1a45c912c1fc5da
bekirytm <bekir.yetim.06@gmail.com>
2020-07-28 05:10:54 +0300

    isAuthenticated middleware

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ const chat = require('./routes/chat');
 
 const app = express();
 
+//helpers
 const db = require('./helpers/db')();
+
+//middleware
+const isAuthenticated = require('./middleware/isAuthenticated');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,7 +36,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET_KEY,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true, maxAge: 14 * 24 * 3600000 }
+  cookie: {maxAge: 14 * 24 * 3600000 }
 }));
 
 //Passport.js
@@ -41,7 +45,7 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/auth', auth);
-app.use('/chat', chat);
+app.use('/chat',isAuthenticated, chat);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
