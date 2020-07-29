8b1d83a88514133207784ea668ffbe0af46654ed
bekirytm <bekir.yetim.06@gmail.com>
2020-07-29 06:46:23 +0300

    socket.io redis adapter

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,3 +1,8 @@
+//CLİENT TARAFI
 app.controller('chatController' , ['$scope' , ($scope) => {
     const socket = io.connect('http://localhost:3000');
+
+    socket.on('sa' , () => {
+        console.log("sa");
+    })
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -1,3 +1,5 @@
+//SERVER TARAFI
+
 const socketio = require('socket.io');
 
 const io = socketio();
@@ -6,8 +8,19 @@ const socketApi = {
     io      //io:io
 };
 
+//Redis Adapter (redis-socket.io)
+
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
+
 io.on('connection' , (socket) => {
     console.log("a user logged in");
+
+    socket.broadcast.emit('sa');
+
 });
 
 module.exports = socketApi;
