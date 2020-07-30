00a4467af4ce20f5c5d6a3a9e4d72fdffb685bfa
bekirytm <bekir.yetim.06@gmail.com>
2020-07-30 04:22:33 +0300

    chat sayfası tab yapısı

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,5 +1,12 @@
 //CLİENT TARAFI
 app.controller('chatController' , ['$scope' , ($scope) => {
+
+    $scope.activeTab = 2;
+
+    $scope.changeTab = (tab) => {
+        $scope.activeTab = tab;
+    };
+
     const socket = io.connect('http://localhost:3000');
 
     socket.on('sa' , () => {
